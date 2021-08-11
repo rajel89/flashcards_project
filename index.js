fb1d16a2e868f1743b65641542809c593a2fe66a
rajel89 <rajah.ello@yahoo.com>
2021-08-11 18:53:30 -0500

    installed and imported express into the index.js.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const connectDB = require('./startup/db');
+const express = requrie('express');
+const app = express();
 
 connectDB ();
 
 
-// const mongoose = require('mongoose');
+app.use (express.json());
 
-//Page 3 of the node_express_mongodb_mongoose_tutorial
-// mongoose
-// .connect('mongodb+srv://new-user-01:YSx371pVxKIbHNnb@<clustername>.6si6q.mongodb.net/flashcard?retryWrites=true&w=majority',
-// { useNewUrlParser: true, useUnifiedTopology: true })
-//  .then(() => console.log('Connected to MongoDB...'))
-//  .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server Start on Port: ${port}`);
+});
 
-
-//code above commented out and replaced by an active code in lines 1 and 2; as shown in in Page 6 of the node_express_mongodb_mongoose_tutorial
 
 
 
